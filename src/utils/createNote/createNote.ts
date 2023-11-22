a6b0495e908c39259fbec998634ded889b26ffdf
Bianca <biancascampos0410@gmail.com>
2023-11-22 09:31:00 -0300

    adding creat note function

---
+++ src/utils/createNote/createNote.ts
@@ -0,0 +1,18 @@
+import { Note } from "../../types/note";
+
+const notes: Note[] = [];
+
+const createNote = () => {
+  // // const { title, date, content, category } = params;
+  // const note = { title: "oi", date: "blá", content: "blá", category: "blá" };
+  // // notes.push({
+  // //   title,
+  // //   date,
+  // //   content,
+  // //   category,
+  // // });
+  // notes.push(note);
+  // console.log(notes);
+};
+
+export default createNote;
